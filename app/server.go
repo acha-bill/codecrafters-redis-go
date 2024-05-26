6ae0cd53745b0e11c98802e047320e189083cd18
Acha Bill <achabill12@gmail.com>
2024-05-26 23:31:15 +0100

    port

--- app/server.go
+++ app/server.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/codecrafters-io/redis-starter-go/app/pkg"
 	"log"
 	"net"
 	"os"
+
+	"github.com/codecrafters-io/redis-starter-go/app/pkg"
 )
+
+var port int
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	flag.IntVar(&port, "port", 6379, "port number")
+	flag.Parse()
+
+	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
