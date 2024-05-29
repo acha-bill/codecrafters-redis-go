73531b84129d8cb86c2f3c36a3384d32efc16b92
Acha Bill <achabill12@gmail.com>
2024-05-29 17:12:24 +0100

    test

--- app/pkg/handler.go
+++ app/pkg/handler.go
@@ -73,7 +73,7 @@ func (h *Set) Handle(sId int64, args []resp.Value, res chan<- []byte) error {
 		return err
 	}
 	h.store.Set(k, v, o.px)
-	fmt.Printf("set %s:%s, store=%s", k, v, h.store.Print())
+	fmt.Printf("set %s:%s, store=%s\n", k, v, h.store.Print())
 	res <- resp.Ok
 	return nil
 }
